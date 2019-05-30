57d7bfe43803bbef0e9b841ec24015aea7bdbe0d
Paulo Henrique Rosa <paulohenriquerosa04@gmail.com>
2019-05-30 18:50:36 -0300

    ...

--- Front_End/gnss_iot/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here. 
-from .models import DataGnss,Receiver
-
-admin.site.register(DataGnss)
-admin.site.register(Receiver)
--- Front_End/gnss_iot/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GnssIotConfig(AppConfig):
-    name = 'gnss_iot'
