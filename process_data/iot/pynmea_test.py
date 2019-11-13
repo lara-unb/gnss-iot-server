9e463b1825f1f6a20d8f87f74628884662460ec8
Raphael Loyola <raphael.braccialli@gmail.com>
2019-11-13 10:19:28 -0300

    nmeas para dataframe on going

--- process_data/iot/pynmea_test.py
+++ process_data/iot/pynmea_test.py
@@ -1,16 +1,42 @@
 import time
 import pynmea2
 import serial
+import pandas as pd
 
 file_path = '../../DATA/EXP1/IOT/logfile.txt'
 
-def read(filename):
+def read2df(filename):
+    df = pd.DataFrame()
     f = open(filename)
     reader = pynmea2.NMEAStreamReader(f)
-
-    while 1:
+    
+    i=0
+    while i<30:
         for msg in reader.next():
-          print(msg.lat)
+            msg_type = msg.sentence_type
+            if msg_type == 'GGA':
+                print('gga')
+            elif msg_type == 'GLL':
+                print('gll')
+            elif msg_type == 'GSA':
+                print('gsa')
+            elif msg_type == 'GSV':
+                print('gsv')
+            elif msg_type == 'RMC':
+                print('rmc')
+            elif msg_type == 'VTG':
+                print('vtg')
+            elif msg_type == 'ZDA':
+                print('zda')
+            elif msg_type == 'TXT':
+                print('txt')
+            else:
+                print(msg_type)
+                
+        i += 1
+        
+    f.close()
+    return df
 
 
 def read_serial(filename):
@@ -31,4 +57,4 @@ def read_serial(filename):
         for msg in reader.next(data):
           print(msg)
 
-read(file_path)
+df = read2df(file_path)
