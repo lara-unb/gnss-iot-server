5037ece925fb2659783ea58f549da3c3fd4edb82
Raphael Braccialli de Loyola <raphael.braccialli@gmail.com>
2019-11-14 18:45:26 -0300

    resolvido index dataframe. Terminar outras mensagens

--- process_data/iot/pynmea_test.py
+++ process_data/iot/pynmea_test.py
@@ -7,63 +7,57 @@ import pandas as pd
 file_path = '../../DATA/EXP1/IOT/logfile.txt'
 
 def read2df(filename):
-    iot_data = {'time':[datetime("00:00:00").time()], 'latitude':[0], 'latitude direction':['S'], 'longitude':[0], 'longitude direction ':['W'], 'quality':[0], 'in use':[0], 'antenna alt':[0], #GGA
+    iot_data = {'time':[], 'latitude':[], 'latitude direction':[], 'longitude':[], 'longitude direction':[], 'quality':[], 'in use':[], 'antenna alt':[], #GGA
             # GLL
-            'DOP':[0], 'HDOP':[0], 'VDOP':[0], #GSA
+            'DOP':[], 'HDOP':[], 'VDOP':[], #GSA
             #RMC
-            'speed kmh':[0], #VTG
-            'date':[datetime("0".date())] #ZDA
+            'speed kmh':[], #VTG
+            'date':[] #ZDA
             }
 
     sat_data = { 'time':[], #GGA
             'PRN':[], #GSA
             'elevation':[], 'azimuth':[], 'SRN':[] # GSV
             }
-<<<<<<< HEAD
     
-    iot_df = pd.DataFrame(iot_data).set_index('time')
-    sat_df = pd.DataFrame(sat_data).set_index('time')
+    iot_df = pd.DataFrame(iot_data)
+    sat_df = pd.DataFrame(sat_data)
     
-    
-=======
 
-    iot_df = pd.DataFrame(data=iot_data).set_index('time')
-    sat_df = pd.DataFrame(data=sat_data).set_index(['time','PRN'])
-
->>>>>>> 2a1b39d10b383de1eda05bac628cc58ebebcd34a
     f = open(filename)
     reader = pynmea2.NMEAStreamReader(f)
 
     #time - update on the go
     i=0
-    while i<100:
+    while i<300:
         for msg in reader.next():
 
             msg_type = msg.sentence_type
 
             if msg_type == 'GGA':
                 time = msg.timestamp
-<<<<<<< HEAD
-                print(time)
-                sat_df = sat_df.append({'time':time}, ignore_index=True)
-                #sat_df.ix[time]=
-                print(sat_df)
-                #print('gga')
-=======
-                if time not in iot_df.index:
 
-                    print(time)
-                    iot_df.append(pd.Series(name=time), ignore_index=False)
-                    print(iot_df)
+                
+                
+                if time not in iot_df['time'].values:
+                    iot_df = iot_df.append({'time':time}, ignore_index=True)
+                
+                
+                iot_df['latitude direction'] = iot_df['latitude direction'].astype('str')
+                iot_df['longitude direction'] = iot_df['longitude direction'].astype('str')
 
-                iot_df[time]['latitude']=msg.lat
-                iot_df[time]['latitude direction']=msg.lat_dir
-                iot_df[time]['longitude']=msg.lon
-                iot_df[time]['longitude direction']=msg.lon_dir
-                iot_df[time]['quality']=msg.gps_qual
-                iot_df[time]['in use']=msg.num_sats
-                iot_df[time]['antenna alt']=msg.altitude
->>>>>>> 2a1b39d10b383de1eda05bac628cc58ebebcd34a
+                time_idx = iot_df[iot_df['time'] == time].index.values.astype(int)[0]
+                
+                
+                print(msg.lon_dir)
+                iot_df.at[time_idx, 'latitude']=msg.lat
+                iot_df.at[time_idx, 'latitude direction']=msg.lat_dir
+                iot_df.at[time_idx, 'longitude']=msg.lon
+                iot_df.at[time_idx, 'longitude direction']=msg.lon_dir
+                iot_df.at[time_idx, 'quality']=msg.gps_qual
+                iot_df.at[time_idx, 'in use']=msg.num_sats
+                iot_df.at[time_idx, 'antenna alt']=msg.altitude
+
             elif msg_type == 'GLL':
                 pass
                 #print('gll')
@@ -92,6 +86,9 @@ def read2df(filename):
         i += 1
 
     f.close()
+    
+    #iot_df = iot_df.set_index('time')
+    #sat_df = sat_df.set_index('time')
     return iot_df, sat_df
 
 
@@ -114,7 +111,4 @@ def read_serial(filename):
           print(msg)
 
 iot_df, sat_df = read2df(file_path)
-<<<<<<< HEAD
-=======
-print(iot_df)
->>>>>>> 2a1b39d10b383de1eda05bac628cc58ebebcd34a
+#print(iot_df)
