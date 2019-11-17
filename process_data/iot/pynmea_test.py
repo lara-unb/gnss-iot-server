ac772615419d196147c08054dbdd4a2b907f8b9d
Raphael Braccialli de Loyola <raphael.braccialli@gmail.com>
2019-11-17 10:40:59 -0300

    prn em ambos dfs

--- process_data/iot/pynmea_test.py
+++ process_data/iot/pynmea_test.py
@@ -7,7 +7,7 @@ import pandas as pd
 file_path = '../../DATA/EXP1/IOT/logfile.txt'
 
 def read2df(filename):
-    iot_data = {'time':[], 'latitude':[], 'latitude direction':[], 'longitude':[], 'longitude direction':[], 'quality':[], 'in use':[], 'antenna alt':[], #GGA
+    iot_data = {'time':[], 'latitude':[], 'latitude direction':[], 'longitude':[], 'longitude direction':[], 'quality':[], 'in use':[],'PRN in use':[], 'antenna alt':[], #GGA
             # GLL
             'PDOP':[], 'HDOP':[], 'VDOP':[], #GSA
             #RMC
@@ -17,7 +17,7 @@ def read2df(filename):
 
     sat_data = { 'time':[], #GGA
             'PRN':[], #GSA
-            'elevation':[], 'azimuth':[], 'SRN':[] # GSV
+            'elevation':[], 'azimuth':[], 'SNR':[] # GSV
             }
     
     iot_df = pd.DataFrame(iot_data)
@@ -26,6 +26,7 @@ def read2df(filename):
     iot_df['latitude direction'] = iot_df['latitude direction'].astype('str')
     iot_df['longitude direction'] = iot_df['longitude direction'].astype('str')
     iot_df['date'] = iot_df['date'].astype('str')
+    iot_df['PRN in use'] = iot_df['PRN in use'].astype(object)
     
 
     f = open(filename)
@@ -33,7 +34,7 @@ def read2df(filename):
 
     #time - update on the go
     i=0
-    while i<4:
+    while i<100:
         for msg in reader.next():
 
             msg_type = msg.sentence_type
@@ -51,7 +52,8 @@ def read2df(filename):
                 iot_df.at[iot_time_idx, 'quality']=msg.gps_qual
                 iot_df.at[iot_time_idx, 'in use']=msg.num_sats
                 iot_df.at[iot_time_idx, 'antenna alt']=msg.altitude
-
+                
+            
             elif msg_type == 'GLL':
                 pass
                 #print('gll')
@@ -60,39 +62,53 @@ def read2df(filename):
                 iot_df.at[iot_time_idx, 'HDOP']=msg.hdop
                 iot_df.at[iot_time_idx, 'VDOP']=msg.vdop
                 
-                if msg.sv_id01:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id01}, ignore_index=True)
-                if msg.sv_id02:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id02}, ignore_index=True)
-                if msg.sv_id03:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id03}, ignore_index=True)
-                if msg.sv_id04:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id04}, ignore_index=True)
-                if msg.sv_id05:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id05}, ignore_index=True)
-                if msg.sv_id06:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id06}, ignore_index=True)
-                if msg.sv_id07:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id07}, ignore_index=True)
-                if msg.sv_id08:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id08}, ignore_index=True)
-                if msg.sv_id09:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id09}, ignore_index=True)
-                if msg.sv_id10:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id10}, ignore_index=True)
-                if msg.sv_id11:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id11}, ignore_index=True)
-                if msg.sv_id12:
-                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_id12}, ignore_index=True)
-                    
-
+                sat_list = []
                 
+                if(msg.sv_id01):
+                    sat_list.append(msg.sv_id01)
+                if(msg.sv_id02):
+                    sat_list.append(msg.sv_id02)
+                if(msg.sv_id03):
+                    sat_list.append(msg.sv_id03)
+                if(msg.sv_id04):
+                    sat_list.append(msg.sv_id04)
+                if(msg.sv_id05):
+                    sat_list.append(msg.sv_id05)
+                if(msg.sv_id06):
+                    sat_list.append(msg.sv_id06)
+                if(msg.sv_id07):
+                    sat_list.append(msg.sv_id07)
+                if(msg.sv_id08):
+                    sat_list.append(msg.sv_id08)
+                if(msg.sv_id09):
+                    sat_list.append(msg.sv_id09)
+                if(msg.sv_id10):
+                    sat_list.append(msg.sv_id10)
+                if(msg.sv_id11):
+                    sat_list.append(msg.sv_id11)
+                if(msg.sv_id12):
+                    sat_list.append(msg.sv_id12)
+                    
+                iot_df.at[iot_time_idx, 'PRN in use'] = sat_list
+                print(iot_df.at[iot_time_idx, 'PRN in use'])
+
+
                 #sat_time_idx = iot_df[iot_df['time'] == time].index.values.astype(int)[0]
                 
                 #sat_df.at[sat_time_idx, 'PRN']=msg.altitude
                     
                 #print('gsa')
             elif msg_type == 'GSV':
+                
+                if msg.sv_prn_num_1:
+                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_prn_num_1, 'elevation':msg.elevation_deg_1, 'azimuth':msg.azimuth_1, 'SNR':msg.snr_1}, ignore_index=True)
+                if msg.sv_prn_num_2:
+                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_prn_num_2, 'elevation':msg.elevation_deg_2, 'azimuth':msg.azimuth_2, 'SNR':msg.snr_2}, ignore_index=True)
+                if msg.sv_prn_num_3:
+                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_prn_num_3, 'elevation':msg.elevation_deg_3, 'azimuth':msg.azimuth_3, 'SNR':msg.snr_3}, ignore_index=True)
+                if msg.sv_prn_num_4:
+                    sat_df = sat_df.append({'time':time, 'PRN': msg.sv_prn_num_4, 'elevation':msg.elevation_deg_4, 'azimuth':msg.azimuth_4, 'SNR':msg.snr_4}, ignore_index=True)
+                    
                 pass
                 #print('gsv')
             elif msg_type == 'RMC':
@@ -144,4 +160,4 @@ def read_serial(filename):
           print(msg)
 
 iot_df, sat_df = read2df(file_path)
-print(sat_df)
+#print(sat_df)
